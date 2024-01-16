47e3ab9e18210bc925c2b677a82b1b53a2d34549
matheus matos da costa <matheus.costa@senac.br>
2024-01-16 15:06:38 -0300

    atualização portfolio

--- js/script.js
+++ js/script.js
@@ -1,10 +1,4 @@
-document.addEventListener("DOMContentLoaded", function () {
-  var menuButton = document.getElementById("menuButton");
-  var menuList = document.getElementById("menuList");
-
-  menuButton.addEventListener("click", function () {
-    // Toggle a class to show/hide the menu
-    menuList.style.display =
-      menuList.style.display === "block" ? "none" : "block";
-  });
-});
+function toggleMenu() {
+  var navbar = document.querySelector(".navbar");
+  navbar.classList.toggle("active");
+}
