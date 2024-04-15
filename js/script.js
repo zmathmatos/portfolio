24f32143a45a23d35063c87a7063676da2a48009
matheus matos da costa <matheus.costa@senac.br>
2024-04-15 15:54:18 -0300

    .

--- js/script.js
+++ js/script.js
@@ -30,3 +30,26 @@ function closeMenu() {
 }
 
 
+document.addEventListener("DOMContentLoaded", function() {
+  const sections = document.querySelectorAll("section");
+  const navLinks = document.querySelectorAll("#side_nav a");
+
+  window.addEventListener("scroll", () => {
+      let current = "";
+      sections.forEach(section => {
+          const sectionTop = section.offsetTop;
+          const sectionHeight = section.clientHeight;
+          if (pageYOffset >= sectionTop - sectionHeight / 3) {
+              current = section.getAttribute("id");
+          }
+      });
+
+      navLinks.forEach(link => {
+          link.parentElement.classList.remove("active");
+          if (link.getAttribute("href").slice(1) === current) {
+              link.parentElement.classList.add("active");
+          }
+      });
+  });
+});
+  
